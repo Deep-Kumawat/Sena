019cb22267bda9318333a31d1eefbb423aa67de1
Mohammed Anees <pvmohammedanees2003@gmail.com>
2026-06-22 23:02:27 +0530

    feat: add more tests to buffer
    Add 2 more test cases
    - insert line
    - delete line
    
    Signed-of-by: DonutAnees <pvmohammedanees2003@gmail.com>

--- test/buffer/main.c
+++ test/buffer/main.c
@@ -33,10 +33,39 @@ static void test_delete_text(void)
     free_buffer(b);
 }
 
+static void test_insert_line(void)
+{
+    buffer *b = load_file_into_buffer("sample.txt");
+
+    insert_line(b, 1);
+
+    size_t len;
+    const char *line = buffer_get_line(b, 1, &len);
+
+    assert(len == 0 && line != NULL); // New line should be empty but not NULL
+
+    free_buffer(b);
+}
+
+static void test_delete_line(void)
+{
+    buffer *b = load_file_into_buffer("sample.txt");
+
+    delete_line(b, 0);
+
+    size_t len;
+    const char *line = buffer_get_line(b, 0, &len);
+
+    assert(len == 0 && line == NULL); // After deleting the first line, the new first line should be empty
+
+    free_buffer(b);
+}
+
 int main(void)
 {
     test_insert_text();
     test_delete_text();
-
+    test_insert_line();
+    test_delete_line();
     return 0;
 }
